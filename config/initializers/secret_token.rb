7897d975c175b7e95861924cc598e2b9132a6d91
Ryan McLeod <ryan.mcleod@airbnb.com>
2013-06-28 10:35:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayApp::Application.config.secret_token = 'cc159886c705c182c73a63c33a1da8d64ac79a53bc4f30f4a3eded2b51d08a5285b7d4e3afaa4bfed5dd007ca62a956cda409fe6675e69c2f33ecbdc572a4ca4'
